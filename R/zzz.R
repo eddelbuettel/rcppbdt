579ef7254dbaaef3d2c73a077d386fccd412cd44
Dirk Eddelbuettel <edd@debian.org>
2011-01-12 20:29:16 +0000

    date constants are now in their own .R file

--- R/zzz.R
+++ R/zzz.R
@@ -27,37 +27,6 @@
 # dummy module, will be replaced later
 bdt <- new( "Module" )
 
-## a simple alternative to enum type in C++ -- we could also have
-## these as parts of a data.frame
-Sun <- 0
-Mon <- 1
-Tue <- 2
-Wed <- 3
-Thu <- 4
-Fri <- 5
-Sat <- 6
-
-## likewise, identifiers for the months
-Jan <- 1
-Feb <- 2
-Mar <- 3
-Apr <- 4
-May <- 5
-Jun <- 6
-Jul <- 7
-Aug <- 8
-Sep <- 9
-Oct <- 10
-Nov <- 11
-Dec <- 12
-
-## and for sequences
-first <- 1
-second <- 2
-third <- 3
-fourth <- 4
-fifth <- 5
-
 .onLoad <- function (lib, pack) {
 
     ## we need the methods package
