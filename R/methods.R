27b3279ee0355c96432661a07b7fcad5976396ab
Dirk Eddelbuettel <edd@debian.org>
2011-01-13 21:57:33 +0000

    added print method

---
+++ R/methods.R
@@ -0,0 +1,23 @@
+##
+## bdt.R: Some accessor functions for Boost Date_Time functionality
+##
+## Copyright (C) 2010 - 2011 Dirk Eddelbuettel and Romain Francois
+##
+## This file is part of RcppBDT.
+##
+## RcppBDT is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## RcppBDT is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with RcppBDT.  If not, see <http://www.gnu.org/licenses/>.
+
+setMethod("print", "Rcpp_date", function(x, ...) print(x$getDate(), ...))
+
+
