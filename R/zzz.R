60de0d8441bf2dc111884193bf4563af669950ad
Dirk Eddelbuettel <edd@debian.org>
2012-06-30 15:58:53 +0000

    updated for R 2.15.1 with new loading mechanism fixed typos in DESCRIPTION and ChangeLog too

--- R/zzz.R
+++ R/zzz.R
@@ -19,25 +19,33 @@
 ## along with RcppBDT.  If not, see <http://www.gnu.org/licenses/>.
 
 
+## .onLoad <- function (lib, pkg) {
+##     req <- get(paste("req", "uire", sep=""))	# we already Imports: methods, but there may
+##     req("methods")  	        		# be a race condition lurking
+##     loadRcppModules(direct=FALSE)
+
+##     setMethod("show", "Rcpp_date", .show_date)
+##     setGeneric("format", function(x,...) standardGeneric("format") )
+##     setMethod("format", "Rcpp_date", .format_date )
+## }
+
+
+loadModule("bdtMod", TRUE)
+
+## create a variable 'bdt' from out bdtMod Module
+## this variable is used as a package-global instance
 delayedAssign( "bdt", local( {
-    x <- new( bdtMod$date )
+    x <- new( date )
     x$setFromUTC()
     x
 }) )
 
-
 .format_date <- function(x, ...) format(x$getDate(), ...)
 .show_date <- function(object) print(object$getDate())
 
-.onLoad <- function (lib, pkg) {
-    req <- get(paste("req", "uire", sep=""))	# we already Imports: methods, but there may
-    req("methods")  	        		# be a race condition lurking
-    loadRcppModules(direct=FALSE)
-
+## define an onLoad expression to set some methods
+evalqOnLoad({
     setMethod("show", "Rcpp_date", .show_date)
     setGeneric("format", function(x,...) standardGeneric("format") )
     setMethod("format", "Rcpp_date", .format_date )
-
-}
-
-
+})
