45c46cb5ebfa6bcb91f6c9958ae3523ac14913e9
Dirk Eddelbuettel <edd@debian.org>
2015-03-17 09:15:41 -0500

    add two more formats and a manual page

--- src/parsePOSIXt.cpp
+++ src/parsePOSIXt.cpp
@@ -25,14 +25,16 @@
 namespace bt = boost::posix_time;
 
 const std::locale formats[] = {
-    std::locale(std::locale::classic(),new bt::time_input_facet("%Y-%m-%d %H:%M:%S%f")),
-    std::locale(std::locale::classic(),new bt::time_input_facet("%Y%m%d %H%M%S%f")),
-    std::locale(std::locale::classic(),new bt::time_input_facet("%Y/%m/%d %H:%M:%S%f")),
-    std::locale(std::locale::classic(),new bt::time_input_facet("%m/%d/%Y %H:%M:%S%f")),
-    std::locale(std::locale::classic(),new bt::time_input_facet("%m-%d-%Y %H:%M:%S%f")),
-    std::locale(std::locale::classic(),new bt::time_input_facet("%d.%m.%Y %H:%M:%S%f")),
-    std::locale(std::locale::classic(),new bt::time_input_facet("%Y-%m-%d")),
-    std::locale(std::locale::classic(),new bt::time_input_facet("%Y%m%d")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%Y-%m-%d %H:%M:%S%f")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%Y%m%d %H%M%S%f")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%Y/%m/%d %H:%M:%S%f")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%m/%d/%Y %H:%M:%S%f")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%m-%d-%Y %H:%M:%S%f")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%d.%m.%Y %H:%M:%S%f")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%Y-%m-%d")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%Y%m%d")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%m/%d/%Y")),
+    std::locale(std::locale::classic(), new bt::time_input_facet("%m-%d-%Y"))
 };
 const size_t nformats = sizeof(formats)/sizeof(formats[0]);
 
@@ -52,6 +54,41 @@ double pt_to_double(const bt::ptime& pt) {
 #endif
 }
 
+//' This function uses the Boost Date_Time library to parse 
+//' datetimes (and dates) from strings. It returns a 
+//' \code{DatetimeVector} ie a vector of \sQuote{POSIXct} 
+//' objects. 
+//'
+//' A numer of fixed formats are tried in succession. These include
+//' the standard ISO format \sQuote{YYYY-MM-DD HH:MM:SS} as well as
+//' different local variants including several forms popular in the 
+//' United States.  Two-digits years and clearly ambigous formats such
+//' as \sQuote{03/04/05} are ignored.  In the case of parsing failure
+//' a \code{NA} value is returned.
+//'
+//' Fractional seconds are supported as well.  As R itself only supports 
+//' microseconds, the Boost compile-time option for nano-second resolution 
+//' has not been enabled.
+//'
+//' @title Parse POSIXct objects from character variables
+//' @param sv A character vector with date(time) expressions
+//' @param tz An optional character variable with a timezone expression, 
+//' default is \sQuote{UTC}
+//' @return A vector of \code{Datetime} objects with \sQuote{POSIXct} elements.
+//' @author Dirk Eddelbuettel
+//' @examples
+//' times <- c("2004-03-21 12:45:33.123456",
+//'           "2004/03/21 12:45:33.123456",
+//'           "20040321 124533.123456",
+//'           "21.03.2004 12:45:33.123456", 
+//'           "03/21/2004 12:45:33.123456",
+//'           "03-21-2004 12:45:33.123456", 
+//'           "2004-03-21",
+//'           "20040321",
+//'           "03/21/2004",
+//'           "03-21-2004")
+//' parsePOSIXt(times)
+//' format(parsePOSIXt(times), tz="UTC")
 // [[Rcpp::export]]
 Rcpp::DatetimeVector parsePOSIXt(Rcpp::CharacterVector sv, const std::string tz="UTC") {
 
